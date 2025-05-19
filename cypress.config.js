c3f5936d5cdb37848c9755f1fafc7b772b7068ea
bijay <bijaykandel37@gmail.com>
2025-05-19 19:39:02 +0545

    installed dot env and setup configure

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,18 @@
 const { defineConfig } = require("cypress");
+const dotenv = require('dotenv');
+
+// Load environment variables from .env file
+dotenv.config();
+
 
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+       config.env.BASE_URL = process.env.BASE_URL;
+       config.env.USERNAME = process.env.USERNAME;
+       config.env.PASSWORD = process.env.PASSWORD;
+       return config;
     },
   },
 });
