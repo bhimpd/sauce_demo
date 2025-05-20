428d58f9b70921a0e238c5510e219f1ea81bbb87
bijay <bijaykandel37@gmail.com>
2025-05-20 20:10:21 +0545

    added mochawesome reporter plugin

--- cypress.config.js
+++ cypress.config.js
@@ -6,8 +6,22 @@ dotenv.config();
 
 
 module.exports = defineConfig({
+  reporter: 'cypress-mochawesome-reporter',
+
+  reporterOptions: {
+    reportDir: "cypress/reports/mochawesome",
+      overwrite: false,
+      html: true,
+      json: true,
+      embeddedScreenshots: true,
+      inlineAssets: true,
+      charts: true,
+      reportPageTitle: "Test Report"
+  },
+  
   e2e: {
     setupNodeEvents(on, config) {
+      require("cypress-mochawesome-reporter/plugin")(on);
       // implement node event listeners here
        config.env.BASE_URL = process.env.BASE_URL;
        config.env.USER_NAME = process.env.USER_NAME;
