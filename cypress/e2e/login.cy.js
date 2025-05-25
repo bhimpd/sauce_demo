91e1663cbaca45d355afdd7639cd7dea12eb4cfa
bhimpd <dreamypd73@gmail.com>
2025-05-25 15:52:18 +0545

    testing

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -4,6 +4,7 @@ import LoginPage from "../support/PageObject/Login/LoginPage";
 
 describe("Login Module", ()=>{
     it("should able to login",()=>{
+        console.log("Running...")
         cy.visitURL();
         LoginPage.loginToDashBoard();
     });
